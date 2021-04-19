609144152c5f3ce7105ea7cdf20a0d27f8334559
Leonardy <leonardyrj@gmail.com>
2021-04-18 22:26:28 -0300

    Datasource

---
+++ micro-catalog/src/datasources/elastic.datasource.ts
@@ -0,0 +1,33 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'elastic',
+  connector: 'es',
+  index: 'catalog',
+  apiVersion: '7',
+  defaultSize: '',
+  configuration: {
+    "node": process.env.ELASTIC_SEARCH_HOST,
+    "requestTimeout": process.env.ELASTIC_SEARCH_REQUEST_TIMEOUT,
+    "pingTimeout": process.env.ELASTIC_SEARCH_PING_TIMEOUT
+  }
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class ElasticDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'elastic';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.elastic', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
